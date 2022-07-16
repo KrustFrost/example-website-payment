8178195f2068c0911dc0f0b7899af5117bfb0b23
Chris <chris@krustfrost.kali>
2022-07-16 17:43:19 +0800

    added forceschema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') === 'production') {
-            \URL::forceSchema('https');
+            \lluminate\Routing\UrlGenerator::forceSchema('https');
         }
     }
 }
