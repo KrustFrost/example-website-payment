3f43324bb5733e1d3ac31136aa24600cf5d7b625
Chris <chris@krustfrost.kali>
2022-07-16 18:06:43 +0800

    added facades

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
-use App\Providers\URL;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
