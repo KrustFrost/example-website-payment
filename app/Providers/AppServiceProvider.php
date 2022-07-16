60d300b5daf420e8ee581a742cd172072cad969e
Chris <chris@krustfrost.kali>
2022-07-16 17:48:34 +0800

    fix forcescheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use App\Providers\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
