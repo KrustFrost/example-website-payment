340fea318cca767afd0aa21d5e9c8f7aa5f6ce6a
Chris <chris@krustfrost.kali>
2022-07-21 17:53:01 +0800

    added nullable database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('course');
+            $table->string('course')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
