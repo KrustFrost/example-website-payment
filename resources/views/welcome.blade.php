33b30706133ccd5e842ba61a53770cd224a3a2a4
Chris <chris@krustfrost.kali>
2022-07-21 17:10:10 +0800

    change the title of the pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>InterPay</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <!-- Fonts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
