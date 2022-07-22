e8c0ce449ea21d83675281f2d63355875d53f2ee
Chris <chris@krustfrost.kali>
2022-07-22 15:57:02 +0800

    remove stripe package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Laravel\Cashier\Billable;
 class User extends Authenticatable
 {
-    use Billable, HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
