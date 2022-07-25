6619d1cb8aa6a0148fd3de73262ef1cc3659694f
Chris <chris@krustfrost.kali>
2022-07-25 14:47:37 +0800

    added cart link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                         @else
                         <button class="btn-lg"><a href="#"><i class="bi bi-cart fa-3x"></i></a></button>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('cart')}}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
