39a39c1f0d655e20ffea23e9d21a417b780b7786
Chris <chris@krustfrost.kali>
2022-07-25 14:50:52 +0800

    fixed cart link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,9 +71,9 @@
                                 </li>
                             @endif
                         @else
-                        <button class="btn-lg"><a href="#"><i class="bi bi-cart fa-3x"></i></a></button>
+                        <button class="btn-lg"><a href="{{{ route('cart')}}"><i class="bi bi-cart fa-3x"></i></a></button>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('cart')}}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
