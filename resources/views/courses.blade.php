1666d0eb1154f9343e8cc46d9d4bd757394a51db
Chris <chris@krustfrost.kali>
2022-07-26 16:42:39 +0800

    added foreach loop

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -6,9 +6,12 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Online Courses') }}</div>
+                @foreach($courses as $course)
                 <form action="/buy" method="POST">
-                    
+                    {{ $course->name }}
+                    {{ $course->description }}
                 <button type="submit">Buy</button>
+                @endforeach
                 </form>
                 <div class="card-body">
                 </div>
